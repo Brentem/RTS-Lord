860fd245591980b1e892c7d2643755e533ecf4eb
John van de Pol <john@jpproducties.nl>
2022-05-14 14:08:49 +0200

    Done for now

--- main.c
+++ main.c
@@ -25,45 +25,52 @@
 
 #include <stdlib.h>
 
+#define VIEWPORT_WIDTH 1920
+#define VIEWPORT_HEIGHT 1080
+
 int main() 
 {
     // Initialization
     //--------------------------------------------------------------------------------------
 
+    // Init for different monitor resolutions => zoom to VIEWPORT_WIDTH/VIEWPORT_HEIGHT
+    InitWindow(VIEWPORT_WIDTH, VIEWPORT_HEIGHT, "raylib");
 
-
-
-
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
-
-    InitWindow(screenWidth, screenHeight, "raylib");
     int currentMonitor = GetCurrentMonitor(); 
     int monitorWidth = GetMonitorWidth(currentMonitor);
     int monitorHeight = GetMonitorHeight(currentMonitor);
     CloseWindow(); 
-
     InitWindow(monitorWidth, monitorHeight, "raylib");
+    ToggleFullscreen();
 
-    float zoomX = monitorWidth/screenWidth;
-    float zoomY = monitorHeight/screenHeight;
+    float zoomX = (float)monitorWidth/VIEWPORT_WIDTH;
+    float zoomY = (float)monitorHeight/VIEWPORT_HEIGHT;
     float zoom = zoomX;
     if(zoomY < zoomX) zoom = zoomY;
-
-    //ToggleFullscreen();
 
 	// setup a camera
 	Camera2D cam = { 0 };
 	cam.zoom = zoom;
 	// center the camera on the middle of the screen
-	cam.offset = (Vector2){ screenWidth / 2, screenHeight / 2 };
+	cam.offset = (Vector2){ monitorWidth / 2, monitorHeight / 2 };
     
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     Texture2D background = Map2DGetBackground();
-    int posX = Map2dGetWidth();
-    int posY = Map2dGetHeight();
+    int mapWidth = Map2dGetWidth();
+    int mapHeight = Map2dGetHeight();
+    int posX = (mapWidth/2) *-1;
+    int posY = (mapHeight/2) *-1;
+
+    int vertikalScrollSpace = mapHeight-VIEWPORT_HEIGHT;
+    int upperBoundary = posY + (vertikalScrollSpace/2);
+    int lowerBoundary = posY - (vertikalScrollSpace/2);
+
+    int horizontalScrollSpace = mapWidth-VIEWPORT_WIDTH;
+    int leftBoundary = posX + (horizontalScrollSpace/2);
+    int rightBoundary = posX - (horizontalScrollSpace/2);
+
     int mouseX;
     int mouseY;
 
@@ -73,24 +80,30 @@ int main()
 
         // Update
         //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT)) posX += 2.0f;
-        if (IsKeyDown(KEY_LEFT)) posX -= 2.0f;
-        if (IsKeyDown(KEY_UP)) posY -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) posY += 2.0f;
+        if (IsKeyDown(KEY_RIGHT)) posX += 5.0f;
+        if (IsKeyDown(KEY_LEFT)) posX -= 5.0f;
+        if (IsKeyDown(KEY_UP)) posY -= 5.0f;
+        if (IsKeyDown(KEY_DOWN)) posY += 5.0f;
 
         mouseX = GetMouseX();
         if (mouseX < 20) posX += 2.0f;
-        if (mouseX < 5) posX += 2.0f;
-
-        if (mouseX > screenWidth-20) posX -= 2.0f;
-        if (mouseX > screenWidth-5) posX -= 2.0f;
+        if (mouseX < 5) posX += 4.0f;
+        if (mouseX > monitorWidth-20) posX -= 2.0f;
+        if (mouseX > monitorWidth-5) posX -= 4.0f;
 
         mouseY = GetMouseY();
         if (mouseY < 20) posY += 2.0f;
         if (mouseY < 5) posY += 2.0f;
+        if (mouseY > monitorHeight-40) posY -= 2.0f;
+        if (mouseY > monitorHeight-25) posY -= 2.0f;
 
-        if (mouseY > screenHeight-20) posY -= 2.0f;
-        if (mouseY > screenHeight-5) posY -= 2.0f;
+        if(posY > upperBoundary) posY = upperBoundary;
+        if(posY < lowerBoundary) posY = lowerBoundary;
+        if(posX > leftBoundary) posX = leftBoundary;
+        if(posX < rightBoundary) posX = rightBoundary;
+     
+
+
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -106,9 +119,9 @@ int main()
 
 		    EndMode2D();
 
-        char snum[5];
-        itoa(mouseX, snum, 10);
-        DrawText(snum, 190, 200, 20, LIGHTGRAY);
+            // char snum[10];
+            // itoa(posY, snum, 10);
+            // DrawText(snum, 190, 200, 20, BLACK);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
